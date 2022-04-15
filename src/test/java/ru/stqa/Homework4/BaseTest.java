efc0c69a4f819e3dfe12a864c875f0f98c27b9f2
Nazilyauser.name <wkwad@mail.ru>
2022-04-16 03:17:52 +0500

    Homework4

---
+++ src/test/java/ru/stqa/Homework4/BaseTest.java
@@ -0,0 +1,63 @@
+package ru.stqa.Homework4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    public EventFiringWebDriver webDriver;
+    public WebDriverWait webDriverWait;
+
+    public boolean isElementPresent(By locator) {
+        //при явном ожидании вызываем операцию поиска до тех пор, пока она не завершится успешно (будет ждать 5 сек - это время указано при создании об-та webDriverWait)
+        try {
+            webDriverWait.until((WebDriver d) -> d.findElement(locator));
+            return true;
+        } catch (TimeoutException ex) {  //функция until выбрасывает уже другое исключение - TimeoutException
+            return false;
+        }
+/*   //при неявном ожидании вызываем операцию поиска - выбрасывает NoSuchElementException
+        try {
+            webDriver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            //} catch (NoSuchElementException ex) {
+            return false;
+        }
+ */
+    }
+    public boolean areElementsPresent(By locator) {
+        return webDriver.findElements(locator).size() > 0;
+    }
+
+    @BeforeAll
+    static void registerDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupBrowser() {
+        webDriver = new EventFiringWebDriver(new ChromeDriver());
+        webDriverWait = new WebDriverWait(webDriver, 10);
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    @AfterEach
+    void tearDown() {
+        webDriver.quit();
+    }
+    public void login() {
+        webDriver.findElement(By.name("username")).sendKeys("admin");
+        webDriver.findElement(By.name("password")).sendKeys("admin");
+        webDriver.findElement(By.name("login")).click();
+    }
+
+}
--- src/test/java/ru/stqa/Homework4/LiteCartUsersPageTest.java
+++ src/test/java/ru/stqa/Homework4/LiteCartUsersPageTest.java
@@ -1,36 +1,70 @@
 package ru.stqa.Homework4;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class LiteCartUsersPageTest {
-    WebDriver driver;
-    WebDriverWait webDriverWait;
-
-    @BeforeAll
-    static void registerDriver() {
-        WebDriverManager.chromedriver().setup();
-    }
+public class LiteCartUsersPageTest extends BaseTest {
 
     @BeforeEach
-    void setUpBrowser() {
-        driver = new ChromeDriver();
-        webDriverWait = new WebDriverWait(driver, 5);
-        driver.get("http://localhost/litecart/");
+    void setupBrowser2() {
+        webDriver.get("http://localhost/litecart/");
     }
 
     @Test
     void UsersMainPageTest() {
+        int stickersCount = 0;
+        int n = 0;
+        //поиск всех уточек на странице
+        List<WebElement> ducks = webDriver.findElements(By.cssSelector("li[class='product column shadow hover-light']"));
+        System.out.println("Всего уточек на странице: " + ducks.size());
+
+        //поиск стикеров в уточках и их подсчет
+        for (int i = 0; i < ducks.size(); i++) {
+            List<WebElement> stickers = ducks.get(i).findElements(By.cssSelector("div.sticker"));
+            n = i + 1;
+            for (int j = 0; j < stickers.size(); j++) {
+                String stickersName = stickers.get(j).getText();
+                System.out.println("У " + n + "-й уточки всего " + stickers.size() + " стикер(ов). Стикер " + stickersName);
+            }
+            Assertions.assertEquals(1, stickers.size());
+            stickersCount = stickersCount + stickers.size();
+        }
+        System.out.println("Всего стикеров на странице: " + stickersCount);
+        Assertions.assertEquals(ducks.size(), stickersCount);
+    }
+}
+
+
+/*
+Хорошо, но над локаторами нужно ещё немного поработать.
+
+1) Для поиска товаров используется локатор с избыточной информацией:
+
+"//ul[@class='listing-wrapper products']//li"
+
+Достаточно вычленить основной, семантически значимый класс, которым помечаются соответствующие товарам элементы,
+и использовать именно его для поиска, а всё несущественное и малозначимое исключить из условия поиска (классы,
+которые используются для оформления)
+
+2) Для поиска стикеров используется локатор, проверяющий частичное совпадение названия класса:
+
+".//div[starts-with(@class,'sticker')]"
+
+Однако полагаться на порядок классов довольно рискованно. Если у элемента указан атрибут class="sticker new"
+это с точки зрения вёрстки то же самое, что и class="new sticker", а локатор перестанет работать. Кроме того,
+верстальщики могут добавить какие-то новые классы, например, class="large sticker new" и это тоже приведёт
+к тому, что локатор перестанет работать, хотя ничего критичного на странице не поменялось. Безопаснее использовать
+функцию contains, а не starts-with.
+
+3) А ещё лучше вместо XPath использовать CSS, локаторы будут более компактные и простые, потому что поиск здесь
+выполняется по классам, а CSS поддерживает краткую нотацию для таких локаторов.
+ */
+
+/*
+    @Test
+    void UsersMainPageTestOld() {
         int stickersCount = 0;
         //поиск всех уточек на странице
         List<WebElement> ducks = driver.findElements(By.xpath("//ul[@class='listing-wrapper products']//li"));
@@ -38,7 +72,7 @@ public class LiteCartUsersPageTest {
 
         //поиск стикеров в уточках и их подсчет
         for (int i = 0; i < ducks.size(); i++) {
-            String ducksName = ducks.get(i).getText();
+            //String ducksName = ducks.get(i).getAttribute("alt");
             List<WebElement> stickers = ducks.get(i).findElements(By.xpath(".//div[starts-with(@class,'sticker')]"));
             for (int j = 0; j < stickers.size(); j++) {
                 String stickersName = stickers.get(j).getText();
@@ -50,9 +84,4 @@ public class LiteCartUsersPageTest {
         System.out.println("Всего стикеров на странице: " + stickersCount);
         Assertions.assertEquals(ducks.size(), stickersCount);
     }
-
-    @AfterEach
-    void stop() {
-        driver.quit();
-    }
-}
+ */
